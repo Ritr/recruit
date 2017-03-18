e7496622bd13176b50da8b002f67130f02371deb
石博文 <http_wenwen@live.com>
2017-03-18 09:52:29 +0800

    修改重构

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,31 @@
-var fs=require("fs"); 
+var fs = require("fs");
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    runSequence = require('run-sequence'),
+    runSequence = require('run-sequence');
 //使用connect启动一个Web服务器
-gulp.task('connect', function () {
-  connect.server();
+gulp.task('connect', function() {
+    connect.server();
 });
 
+gulp.task('fileincludeAdmin', function() {
+    gulp.src("admin/html/*.html")
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('admin/html2'));
+});
+
+gulp.task('fileincludePerson', function() {
+    gulp.src("person/html/*.html")
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('person/html2'));
+});
+
+gulp.task('fileinclude', function() {
+    runSequence("fileincludeAdmin","fileincludeAdmin");
+})
